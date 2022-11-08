97f575cd0f2a4fb8e1b4234bf57b42b47da3e117
Luis Perez Miguel <luisperezmiguel1@gmail.com>
2022-11-08 10:45:12 +0100

    Voy a intentarlo en mi portatil

--- analyzer.py
+++ analyzer.py
@@ -7,34 +7,56 @@ import datetime
 from flair.data import Sentence
 from flair.models.sequence_tagger_model import SequenceTagger
 
+import paramiko
+
+if not os.environ.get('GICSERVER_PW'):
+    raise ValueError('GICSERVER_PW env variable not set.')
+
+
 THREADS_DIR = 'threads'
 DATA_DIR = '/home/luis/data/4chan'
 BOARDS_DIR = 'boards'
 SEARCH_DIR = 'searchs'
-ANALYSIS_DIR = 'analysis'
+ANALYSIS_DIR = 'data/4chan/analysis'
 
 WANTED_BOARDS = ['pol']
 
+BATCH_COMMENTS_SIZE = 100
 
 tagger = SequenceTagger.load("ner-fast")
 
-def full_board_texts(boards_list):
+def connect_ssh():
+    ssh_client=paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname='gicserver',username='luis',password=os.environ.get('GICSERVER_PW'))
+    return ssh_client
+
+def full_board_texts(boards_list, ssh_client):
     boards_dp = os.path.join(DATA_DIR, BOARDS_DIR)
+    text_dict = {}
 
     for boardname in boards_list:
-    
+        text_list = []
         board_dp = os.path.join(boards_dp, f'{boardname}/')
         thread_dp = os.path.join(board_dp, THREADS_DIR)
+        ftp_client = ssh_client.open_sftp()
+        for file in ftp_client.listdir(thread_dp):
+            text_dict[boardname]=[]
+            f = ftp_client.open(os.path.join(thread_dp, file), "r")
+            dict_list = json.load(f)
+            for th_dict in dict_list:
+                if 'sub' in th_dict:
+                    text_list.append(th_dict['sub'])
+                if 'com' in th_dict:
+                    text_list.append(th_dict['com'])
+            f.close()
+        ftp_client.close()
+        text_dict[boardname] = text_list
 
-        for file in os.listdir(thread_dp):
-            with open(os.path.join(thread_dp, file), "r") as f:
-                dict_list = json.load(f)
-                for th_dict in dict_list:
-                    if 'sub' in th_dict:
-                        text_list.append(th_dict['sub'])
-                    if 'com' in th_dict:
-                        text_list.append(th_dict['com'])
-    
+    return text_dict
+
+
+def clean_text(text_list):
     text = "\n".join(text_list)
 
     # Remove quotes signifiers
@@ -44,19 +66,17 @@ def full_board_texts(boards_list):
     soup = BeautifulSoup(text,'html.parser')
     return soup.get_text()
 
-
 def _sort_dict(unsorted_dict):
     for label, value in unsorted_dict.items():
         unsorted_dict[label] = dict(sorted(value.items(), key=lambda item: item[1], reverse=True))
     return unsorted_dict
 
 
-def ner_text(text):
+def ner_text(text, entities={}):
 
     sentence = Sentence(text)
     tagger.predict(sentence)
 
-    entities = {}
     for span in sentence.get_spans("ner"):
 
         if span.tag not in entities:
@@ -76,11 +96,16 @@ if __name__ == '__main__':
         os.mkdir(BOARDS_DIR)
     if not os.path.exists(ANALYSIS_DIR):
         os.mkdir(ANALYSIS_DIR)
-    res = ner_text(full_board_texts(WANTED_BOARDS))
-    res = _sort_dict(res)
-
-
+    ssh_client = connect_ssh()
+    text_dict = full_board_texts(WANTED_BOARDS,ssh_client)
+    entities = {}
+    for item, value in text_dict.items():
+        for i in range(0, len(value), BATCH_COMMENTS_SIZE):
+            chunks = value[i:i+BATCH_COMMENTS_SIZE] 
+            entities = ner_text(clean_text(chunks), entities=entities)
+            print("CHUNK")
+    entities = _sort_dict(entities)
     now = datetime.now()
     now_str = now.strftime("%d-%m-%Y_%H:%M:%S")
     with open(os.path.join(ANALYSIS_DIR, f'analysis_{now_str}.json'), 'w') as file:
-        json.dump(res, file, indent=4)
+        json.dump(entities, file, indent=4)
